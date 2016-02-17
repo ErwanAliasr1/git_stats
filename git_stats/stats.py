3e6aa236d100a67d8c610d9bdeaf484d72cfde81
Erwan Velu <erwan@redhat.com>
2016-02-17 14:24:57 +0100

    stats: Fixing main call()
    This change broke a local execution. Shouldn't have been comitted.

--- git_stats/stats.py
+++ git_stats/stats.py
@@ -78,7 +78,8 @@ def compute_authors_stats(commits):
         authors.append(commit[commit.keys()[0]])
     return Counter(authors)
 
-def main():
+
+if __name__ == '__main__':
     url = None
     contributors_count = DEFAULT_CONTRIBUTOR_COUNT
     try:
